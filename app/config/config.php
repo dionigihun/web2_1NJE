cafa9da5c5ee800c60b73b45efc80949825abbf6
oláhDénes <dionigi.hun@gmail.com>
2022-11-29 21:05:28 +0100

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'szoftleltarnje');
+define('DB_PASS', 'szoftleltarnje');
+define('DB_NAME', 'szoftleltarnje');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://www.olahdenes.nhely.hu/');
+// Site Name
+define('SITENAME', 'Logismatiko Kft.');
+// App Version
+define('APPVERSION', '1.0');
+
---
+++ app/views/inc/aside.php
@@ -0,0 +1,16 @@
+<aside class="col-md-4 blog-sidebar rounded">
+    <div class="p-3 mb-3 rounded d-flex justify-content-center align-items-center">
+        <img width="80%" src="<?php echo URLROOT; ?>img/image-sidebar.jpg" alt="">
+    </div>
+
+    <div class="p-3">
+        <h4 class="font-italic mb-4">Webprogramozás 2 beadandó</h4>
+        <p>
+            Tagok: <strong>Oláh Dénes és Földházi Balázs</strong><br>
+            Adatbázis: <strong>Szoftverleltár</strong>
+        </p>
+        <ol class="list-unstyled">
+            <li><a href="https://github.com/dionigihun/web2_1NJE" target="_blank">GitHub repository</a></li>
+        </ol>
+    </div>
+</aside>
